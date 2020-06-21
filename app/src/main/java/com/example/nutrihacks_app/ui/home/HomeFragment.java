9721268f383def87a645b4f787e7f437a261ffee
Kostas Chikimtzis <kostaschikis@gmail.com>
2020-06-21 16:45:16 +0300

    added cover on home page

--- app/src/main/java/com/example/nutrihacks_app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/nutrihacks_app/ui/home/HomeFragment.java
@@ -16,24 +16,14 @@ import com.example.nutrihacks_app.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-        // View Model
-        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
 
         // Inflate the View Layout
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
