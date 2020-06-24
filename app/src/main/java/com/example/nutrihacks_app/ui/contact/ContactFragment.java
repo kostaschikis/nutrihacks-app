593aa39924fd24f338bd8b226ff3607dbfb974fc
Kostas Chikimtzis <kostaschikis@gmail.com>
2020-06-24 15:57:25 +0300

    Contact Fragment

---
+++ app/src/main/java/com/example/nutrihacks_app/ui/contact/ContactFragment.java
@@ -0,0 +1,32 @@
+package com.example.nutrihacks_app.ui.contact;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.nutrihacks_app.R;
+
+import org.w3c.dom.Text;
+
+public class ContactFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the View Layout
+        View root = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        final TextView textView = root.findViewById(R.id.text_contact);
+        textView.setText("Contact Page");
+
+        return root;
+    }
+}
