d5d5ef67f7d8026995354edbdb87631d4ea2eab9
Kostas Chikimtzis <kostaschikis@gmail.com>
2020-06-27 16:09:18 +0300

    Contact Fragment Working

--- app/src/main/java/com/example/nutrihacks_app/ui/contact/ContactFragment.java
+++ app/src/main/java/com/example/nutrihacks_app/ui/contact/ContactFragment.java
@@ -5,22 +5,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+
 
 import com.example.nutrihacks_app.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import org.w3c.dom.Text;
 
 public class ContactFragment extends Fragment {
+    // Firebase Database Instance
     DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+    // Save Button
+    private Button btnsave;
+    // User Input
+    private EditText firstName, lastName, email, age;
+    private long uid = 1;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -29,25 +34,38 @@ public class ContactFragment extends Fragment {
         // Inflate the View Layout
         View root = inflater.inflate(R.layout.fragment_contact, container, false);
 
-        final Button btnsave = root.findViewById(R.id.btnSave);
+        // Fetch Button Views
+        btnsave = root.findViewById(R.id.btnSave);
+        // Fetch User Input Views
+        firstName = (EditText) root.findViewById(R.id.firstName);
+        lastName = (EditText) root.findViewById(R.id.lastName);
+        email = (EditText) root.findViewById(R.id.email);
+        age = (EditText) root.findViewById(R.id.age);
+
+        // Save Button Listener
         btnsave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name = "kostas";
-                String email = "kostachikis@gmail.com";
-                writeNewUser("1", name, email);
+                writeNewUser(String.valueOf(uid),
+                        firstName.getText().toString().trim(),
+                        lastName.getText().toString().trim(),
+                        email.getText().toString().trim(),
+                        age.getText().toString().trim()
+                );
             }
         });
 
-        // final TextView textView = root.findViewById(R.id.text_contact);
-        // textView.setText("Contact Page");
         return root;
     }
 
-    private void writeNewUser(String userId, String name, String email) {
-        User user = new User(name, email);
+    private void writeNewUser(String userId, String firstName, String lastName, String email, String age) {
+        // Make a new user object
+        User user = new User(firstName, lastName, email, age);
 
         database.child("users").child(userId).setValue(user);
+        Toast.makeText(getActivity(), "User inserted successfully",Toast.LENGTH_LONG).show();
+
+        uid++;
     }
 
 
--- app/src/main/java/com/example/nutrihacks_app/ui/contact/User.java
+++ app/src/main/java/com/example/nutrihacks_app/ui/contact/User.java
@@ -2,16 +2,20 @@ package com.example.nutrihacks_app.ui.contact;
 
 public class User {
 
-    public String username;
+    public String firstName;
+    public String lastName;
     public String email;
+    public String age;
 
     public User() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public User(String username, String email) {
-        this.username = username;
+    public User(String firstName, String lastName, String email, String age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
+        this.age = age;
     }
 
 }
